7d462bb75d1305e2f546bb024ee77073b78e05b6
jtorresli <manu.jose.torres@gmail.com>
2020-06-26 00:04:34 -0500

    feat: version final

--- Practica 5/ejercicio1.py
+++ Practica 5/ejercicio1.py
@@ -20,6 +20,29 @@ def substitution_matrix(file):
 
 	return r_c, s
 
+def save_result(F):
+	f = open(result_file, "w")
+
+	f.write('              ')
+	for i in range(column-1):
+		f.write(seq1[i]+'        ')
+
+	f.write('\n')	
+
+	seq2_t = ' '+ seq2
+
+	for r in range(row):
+		f.write(seq2_t[r])
+		for c in range(column):			
+			f.write(' ( '+ '{}'.format(F[r][c][0])+' '+'{}'.format(F[r][c][1])+') ')
+
+		f.write('\n')
+	f.close()	
+
+	f = open(result_file, "r")
+	print(f.read())
+
+
 def get_score(seq1, seq2):
 	score = 0
 	for i in range(len(seq1)):
@@ -31,28 +54,39 @@ def get_score(seq1, seq2):
 				print ('(',d,')', end='', sep='')
 				score = score + d
 			else:
-				print ('(',s[r_c[seq2[i]]][r_c[seq1[j]]] ,')', end='', sep='')		
+				print ('(',s[r_c[seq2[i]]][r_c[seq1[i]]] ,')', end='', sep='')		
 				score = score + s[r_c[seq2[i]]][r_c[seq1[i]]]
 
 		print ('+', end='', sep='')
 
 	print("\b",end="") 		
-	print ('=', score)					
+	print ('=', score)	
+ 
+			
+def get_sequences(F, i, j, alignmented_seq1 = "", alignmented_seq2 = ""):
 
-def get_sequences(F, i, j):
-	
-	alignmented_seq1 = ""
-	alignmented_seq2 = ""
+	if F[i][j][1]==0:
+		print (alignmented_seq1)
+		print (alignmented_seq2)
+		get_score(alignmented_seq1, alignmented_seq2)
+		print ()	
+		return
+			
+	if len(F[i][j][1])>1:
+		directions = F[i][j][1]
+		for n in range(len(directions)):	
+			F[i][j][1] = directions[n]
+			get_sequences(F, i, j, alignmented_seq1, alignmented_seq2)			
 
-	while (i > 0 or j > 0):
-
-		if (i>0 and j>0 and F[i][j][1] == 'DIAG'):		
+	else:	
+		
+		if F[i][j][1] == 'D':		
 			alignmented_seq1 = seq1[j-1] + alignmented_seq1
 			alignmented_seq2 = seq2[i-1] + alignmented_seq2
 			i = i-1
 			j = j-1
 
-		elif (i>0 and F[i][j][1]=='UP'):
+		elif F[i][j][1]=='U':
 			alignmented_seq1 = "-" + alignmented_seq2	
 			alignmented_seq2 = seq2[i-1] + alignmented_seq2
 			i = i-1
@@ -60,17 +94,14 @@ def get_sequences(F, i, j):
 		else:
 			alignmented_seq2 = "-" + alignmented_seq2
 			alignmented_seq1 = seq1[j-1] + alignmented_seq1
-			j = j-1	   
-			
-	print ()		
-	print (alignmented_seq1)
-	print (alignmented_seq2)	
-	get_score(alignmented_seq1, alignmented_seq2)
+			j = j-1
+
+		get_sequences(F, i, j, alignmented_seq1, alignmented_seq2)			
 
 
 def global_alignment(F, i, j):
 
-	number_roads = []
+	directions = ''
 
 	diag = F[i-1][j-1][0] + s[r_c[seq2[i-1]]][r_c[seq1[j-1]]]
 	up = F[i-1][j][0] + d
@@ -78,36 +109,33 @@ def global_alignment(F, i, j):
 
 	F[i][j][0]	= max(diag, up, left)
 	
+	if F[i][j][0]==left:	
+		directions = directions + 'L'
+
 	if F[i][j][0]==diag:
-		#F[i][j][1] = 'DIAG'			
-		number_roads.append('DIAG')
+		directions = directions + 'D'
 
 	if F[i][j][0]==up:	
-		#F[i][j][1] = 'UP'
-		number_roads.append('UP')
+		directions = directions + 'U'	
 	
-	if F[i][j][0]==left:	
-		#F[i][j][1] = 'LEFT'
-		number_roads.append('LEFT')
-	
-	for n in range(len(number_roads)):
-		F[i][j][1] = number_roads[n]
-		if i==row-1 and j==column-1:
-			print ()
-			print (F)
-			get_sequences(F, i, j)
-			return
+	F[i][j][1] = directions
 
-		elif j<column-1:
-			global_alignment(F, i ,j+1)	
-		else:	
-			global_alignment(F, i+1 ,1)							
+	if i==row-1 and j==column-1:
+		save_result(F)
+		get_sequences(F, i, j)
+		return
+
+	elif j<column-1:
+		global_alignment(F, i ,j+1)	
+	else:	
+		global_alignment(F, i+1 ,1)							
 
 
 if __name__ == "__main__":
 
 	filename = sys.argv[1]	
 	d = int(sys.argv[2])
+	result_file = "resultado.txt"
 	
 	file = open(filename, "r")
 
@@ -116,9 +144,6 @@ if __name__ == "__main__":
 
 	seq1 = "AAG"
 	seq2 = "AGC"	
-
-	#identicalMatch = 1
-	#mismatch = -1
 
 	# zeros column and row at the beginning of matrix F
 	column = len(seq1)+1
@@ -129,10 +154,10 @@ if __name__ == "__main__":
 	# Adding zeros 
 	for i in range(1,column):
 		F[0][i][0] = i*d
-		F[0][i][1] = 'LEFT'
+		F[0][i][1] = 'L'
 
 	for i in range(1,row):
 		F[i][0][0] = i*d
-		F[i][0][1] = 'UP'
+		F[i][0][1] = 'U'
 
 	global_alignment(F, 1, 1)	
