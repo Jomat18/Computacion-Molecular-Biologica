b2a90314db944a09971a90d74e99d9107cdae80e
jtorresli <manu.jose.torres@gmail.com>
2020-06-26 11:35:11 -0500

    feat: mostrando todas las secuencias

--- Practica 7/ejercicio1.py
+++ Practica 7/ejercicio1.py
@@ -21,25 +21,47 @@ def substitution_matrix(file):
 
 	return r_c, s
 
+
+def save_result(F):
+	f = open(result_file, "w")
+
+	f.write('            ')
+	for i in range(column-1):
+		f.write(seq1[i]+'        ')
+
+	f.write('\n')	
+
+	seq2_t = ' '+ seq2
+
+	for r in range(row):
+		f.write(seq2_t[r])
+		for c in range(column):			
+			f.write(' ( '+ '{}'.format(F[r][c][0])+' '+'{}'.format(F[r][c][1])+') ')
+
+		f.write('\n')
+	f.close()	
+
+	f = open(result_file, "r")
+	print(f.read())	
+
 def get_sequences(F, i, j, alignmented_seq1 = "", alignmented_seq2 = ""):
 
 	if F[i][j][0]==0:
 		print ()		
 		print (alignmented_seq1)
-		print ()
 		print (alignmented_seq2)	
 		return
 			
 	
 	if i > 0 or j > 0:			
 		
-		if (i>0 and j>0 and F[i][j][1] == 'DIAG'):		
+		if (i>0 and j>0 and F[i][j][1] == 'D'):		
 			alignmented_seq1 = seq1[j-1] + alignmented_seq1
 			alignmented_seq2 = seq2[i-1] + alignmented_seq2
 			i = i-1
 			j = j-1
 
-		elif (i>0 and F[i][j][1]=='UP'):
+		elif (i>0 and F[i][j][1]=='U'):
 			alignmented_seq1 = "-" + alignmented_seq2	
 			alignmented_seq2 = seq2[i-1] + alignmented_seq2
 			i = i-1
@@ -61,27 +83,31 @@ def local_alignment(F, i, j):
 	F[i][j][0]	= max(diag, up, left, 0)
 	
 	if F[i][j][0]==diag:
-		F[i][j][1] = 'DIAG'	
+		F[i][j][1] = 'D'	
 
 	if F[i][j][0]==up:	
-		F[i][j][1] = 'UP'
+		F[i][j][1] = 'U'
 	
 	if F[i][j][0]==left:	
-		F[i][j][1] = 'LEFT'
+		F[i][j][1] = 'L'
 
 	if i==row-1 and j==column-1:
-		print ()
-		print (F)
+		save_result(F)
 
-		mayor = -1000
+		major = -1000
+		s_i = 0
+		s_j = 0 
 		for r in range(1,row):
 			for c in range(1, column):
-				if F[r][c][0]>mayor:
+				if F[r][c][0]>major:
+					s_i = i
+					s_j = j
 					i = r
 					j = c
-					mayor = F[r][c][0]
+					major = F[r][c][0]
 
 		get_sequences(F, i, j)
+		get_sequences(F, s_i, s_j)
 		return
 
 	if j<column-1:
@@ -94,6 +120,8 @@ if __name__ == "__main__":
 
 	filename = sys.argv[1]	
 	d = int(sys.argv[2])
+
+	result_file = "resultado.txt"
 	
 	file = open(filename, "r")
 
